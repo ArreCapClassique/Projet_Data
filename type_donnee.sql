5c5c102d315fb33f975c83a502f9df1f7043743d
乃0你带我走吧 <57453310+ArreCapClassique@users.noreply.github.com>
2025-05-14 16:08:53 +0200

    fixed bug

--- type_donnee.sql
+++ type_donnee.sql
@@ -1,32 +1,10 @@
-ALTER TABLE stg.transactions
-    ALTER COLUMN mnt_depassement   SET DATA TYPE FLOAT,
-    ALTER COLUMN mnt_pay           SET DATA TYPE FLOAT,
-    ALTER COLUMN mnt_rem           SET DATA TYPE FLOAT,
-    ALTER COLUMN ann_soin          SET DATA TYPE INTEGER,
-    ALTER COLUMN mois_soin         SET DATA TYPE INTEGER,
-    ALTER COLUMN age               SET DATA TYPE VARCHAR(20),
-    ALTER COLUMN region_ben        SET DATA TYPE VARCHAR(100),
-    ALTER COLUMN sexe              SET DATA TYPE VARCHAR(50),
-    ALTER COLUMN ticket_mod        SET DATA TYPE VARCHAR(100),
-    ALTER COLUMN nat_assurance     SET DATA TYPE VARCHAR(200),
-    ALTER COLUMN type_envlp        SET DATA TYPE VARCHAR(200),
-    ALTER COLUMN nat_destinataire  SET DATA TYPE VARCHAR(200),
-    ALTER COLUMN motif_exo         SET DATA TYPE VARCHAR(200),
-    ALTER COLUMN nat_prestation    SET DATA TYPE VARCHAR(200),
-    ALTER COLUMN code_secteur      SET DATA TYPE VARCHAR(10),
-    ALTER COLUMN taux_remb         SET DATA TYPE FLOAT,
-    ALTER COLUMN type_remb         SET DATA TYPE VARCHAR(50);
-
-    
-CREATE OR REPLACE TABLE stg.month1_libelles AS
+CREATE OR REPLACE TABLE stg.transactions_libelles AS
 SELECT
     mnt_depassement,
     mnt_pay,
     mnt_rem,
     ann_soin,
     mois_soin,
-
-    -- Tranche d'âge (libellé)
     CASE
         WHEN age = '0'  THEN '0-19 ANS'
         WHEN age = '20' THEN '20 - 29 ANS'
@@ -39,10 +17,8 @@ SELECT
         WHEN age = '99' THEN 'AGE INCONNU'
         ELSE NULL
     END AS age,
-
-    -- Région de résidence (libellé)
     CASE
-        WHEN region_ben = '5'  THEN 'Régions et Départements d\'outre-mer'
+        WHEN region_ben = '5'  THEN 'Régions et Départements d''outre-mer'
         WHEN region_ben = '11' THEN 'Ile-de-France'
         WHEN region_ben = '24' THEN 'Centre-Val de Loire'
         WHEN region_ben = '27' THEN 'Bourgogne-Franche-Comté'
@@ -54,12 +30,10 @@ SELECT
         WHEN region_ben = '75' THEN 'Aquitaine-Limousin-Poitou-Charentes'
         WHEN region_ben = '76' THEN 'Languedoc-Roussillon-Midi-Pyrénées'
         WHEN region_ben = '84' THEN 'Auvergne-Rhône-Alpes'
-        WHEN region_ben = '93' THEN 'Provence-Alpes-Côte d\'Azur et Corse'
+        WHEN region_ben = '93' THEN 'Provence-Alpes-Côte d''Azur et Corse'
         WHEN region_ben = '99' THEN 'Inconnu'
         ELSE NULL
     END AS region_ben,
-
-    -- Sexe (libellé)
     CASE
         WHEN sexe = '0' THEN 'INCONNU OU PERSONNE MORALE SANS CIVILITE'
         WHEN sexe = '1' THEN 'MASCULIN'
@@ -67,8 +41,6 @@ SELECT
         WHEN sexe = '9' THEN 'VALEUR INCONNUE'
         ELSE NULL
     END AS sexe,
-
-    -- Nature d'assurance (libellé)
     CASE
         WHEN nat_assurance = '0'  THEN 'SANS OBJET'
         WHEN nat_assurance = '10' THEN 'MALADIE'
@@ -84,23 +56,20 @@ SELECT
         WHEN nat_assurance = '99' THEN 'VALEUR INCONNUE'
         ELSE NULL
     END AS nat_assurance,
-
-    -- Type d'enveloppe (libellé)
     CASE
-        WHEN type_env = '0'  THEN 'PRESTATIONS NON PRISE EN CHARGE PAR LE RG'
-        WHEN type_env = '1'  THEN 'SOINS DE VILLE'
-        WHEN type_env = '2'  THEN 'HOSPITALISATION ET MEDICO-SOCIALE ET CONSOMMATIONS INTERMEDIAIRES (MEDICAMENTS ET LPP)'
-        WHEN type_env = '3'  THEN 'PRESTATIONS LEGALES ASSURANCE MALADIE HORS ENV.ONDAM'
-        WHEN type_env = '4'  THEN 'PREVENTION'
-        WHEN type_env = '5'  THEN 'FNAS'
-        WHEN type_env = '6'  THEN 'IVG (VALEUR SUPPRIMEE)'
-        WHEN type_env = '7'  THEN 'C2S, AME ou ACS'
-        WHEN type_env = '8'  THEN 'ALSACE MOSELLE'
-        WHEN type_env = '9'  THEN 'VALEUR INCONNUE'
-        WHEN type_env = '98' THEN 'SANS OBJET'
+        WHEN type_envlp = '0'  THEN 'PRESTATIONS NON PRISE EN CHARGE PAR LE RG'
+        WHEN type_envlp = '1'  THEN 'SOINS DE VILLE'
+        WHEN type_envlp = '2'  THEN 'HOSPITALISATION ET MEDICO-SOCIALE ET CONSOMMATIONS INTERMEDIAIRES (MEDICAMENTS ET LPP)'
+        WHEN type_envlp = '3'  THEN 'PRESTATIONS LEGALES ASSURANCE MALADIE HORS ENV.ONDAM'
+        WHEN type_envlp = '4'  THEN 'PREVENTION'
+        WHEN type_envlp = '5'  THEN 'FNAS'
+        WHEN type_envlp = '6'  THEN 'IVG (VALEUR SUPPRIMEE)'
+        WHEN type_envlp = '7'  THEN 'C2S, AME ou ACS'
+        WHEN type_envlp = '8'  THEN 'ALSACE MOSELLE'
+        WHEN type_envlp = '9'  THEN 'VALEUR INCONNUE'
+        WHEN type_envlp = '98' THEN 'SANS OBJET'
         ELSE NULL
-    END AS type_env,
-
+    END AS type_envlp,
     CASE
         WHEN ticket_mod = '0'  THEN 'PRESTATION DE REFERENCE'
         WHEN ticket_mod = '1'  THEN 'COMPLEMENT D ACTE'
@@ -144,8 +113,6 @@ SELECT
         WHEN motif_exo = '99'  THEN 'VALEUR INCONNUE'
         ELSE NULL
     END AS motif_exo,
-
-    -- Nature du Destinataire du Règlement Affiné (libellé)
     CASE
         WHEN nat_destinataire = '0'   THEN 'SANS OBJET'
         WHEN nat_destinataire = '11'  THEN 'Assuré'
@@ -175,45 +142,10 @@ SELECT
         WHEN nat_destinataire = '99'  THEN 'VALEUR INCONNUE'
         ELSE NULL
     END AS nat_destinataire,
-
     CASE
         WHEN code_secteur = '1' THEN 'PUBLIC'
         WHEN code_secteur = '2' THEN 'PRIVE'
         WHEN code_secteur = '9' THEN 'INCONNUE'
         ELSE NULL
-    END AS code_secteur,
-
-  
-    CASE
-        WHEN nat_assurance = '0'  THEN 'SANS OBJET'
-        WHEN nat_assurance = '10' THEN 'MALADIE'
-        WHEN nat_assurance = '11' THEN 'MALADIE COURS NAVIGATION > 6 MOIS'
-        WHEN nat_assurance = '12' THEN 'MALADIE COURS NAVIGATION < 6 MOIS'
-        WHEN nat_assurance = '22' THEN 'SOINS AUX INVALIDES DE GUERRE (CNMSS)'
-        WHEN nat_assurance = '30' THEN 'MATERNITE'
-        WHEN nat_assurance = '40' THEN 'AT ET MP'
-        WHEN nat_assurance = '50' THEN 'DECES'
-        WHEN nat_assurance = '70' THEN 'PRESTATIONS SUPPLEMENTAIRES'
-        WHEN nat_assurance = '80' THEN 'INVALIDITE'
-        WHEN nat_assurance = '90' THEN 'PREVENTION MALADIE'
-        WHEN nat_assurance = '99' THEN 'VALEUR INCONNUE'
-        ELSE NULL
-    END AS nat_assurance
-  
-    CASE
-        WHEN type_env = '0'  THEN 'PRESTATIONS NON PRISE EN CHARGE PAR LE RG'
-        WHEN type_env = '1'  THEN 'SOINS DE VILLE'
-        WHEN type_env = '2'  THEN 'HOSPITALISATION ET MEDICO-SOCIALE ET CONSOMMATIONS INTERMEDIAIRES (MEDICAMENTS ET LPP)'
-        WHEN type_env = '3'  THEN 'PRESTATIONS LEGALES ASSURANCE MALADIE HORS ENV.ONDAM'
-        WHEN type_env = '4'  THEN 'PREVENTION'
-        WHEN type_env = '5'  THEN 'FNAS'
-        WHEN type_env = '6'  THEN 'IVG (VALEUR SUPPRIMEE)'
-        WHEN type_env = '7'  THEN 'C2S, AME ou ACS'
-        WHEN type_env = '8'  THEN 'ALSACE MOSELLE'
-        WHEN type_env = '9'  THEN 'VALEUR INCONNUE'
-        WHEN type_env = '98' THEN 'SANS OBJET'
-        ELSE NULL
-    END AS type_env,
-
+    END AS code_secteur
 FROM stg.transactions;
-
