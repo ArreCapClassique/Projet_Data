ef35daefd1d13c30ca412a8ddce404037f6bc847
ddeguene <ddgijoe0@gmail.com>
2025-05-15 13:48:08 +0200

    dim à modifier

--- 3. dwh.sql
+++ 3. dwh.sql
@@ -7,7 +7,7 @@ SELECT
     ann_soin, mois_soin
 FROM (
     SELECT DISTINCT ann_soin, mois_soin
-    FROM stg.month1
+    FROM stg.transactions_libelles
 );
 
 CREATE OR REPLACE TABLE dwh.dim_beneficiaire AS
@@ -17,19 +17,17 @@ SELECT
     age, region_ben, sexe, ticket_mod
 FROM (
     SELECT DISTINCT age, region_ben, sexe, ticket_mod
-    FROM stg.month1
+    FROM stg.transactions_libelles
 );
-
-FROM dwh.dim_beneficiaire;
 
 CREATE OR REPLACE TABLE dwh.dim_prestation AS
 SELECT
-    ROW_NUMBER() OVER (ORDER BY nat_assurance, nat_accident, type_env, nat_destinataire, motif_exo, nat_prestation, code_secteur, taux_remb, type_remb) AS id_prestation,
-    MD5(CONCAT(nat_assurance, nat_accident, type_env, nat_destinataire, motif_exo, nat_prestation, code_secteur, taux_remb, type_remb)) AS cd_prestation,
-    nat_assurance, nat_accident, type_env, nat_destinataire, motif_exo, nat_prestation, code_secteur, taux_remb, type_remb
+    ROW_NUMBER() OVER (ORDER BY nat_assurance, type_env, nat_destinataire, motif_exo, nat_prestation, code_secteur, taux_remb, type_remb) AS id_prestation,
+    MD5(CONCAT(nat_assurance, type_env, nat_destinataire, motif_exo, nat_prestation, code_secteur, taux_remb, type_remb)) AS cd_prestation,
+    nat_assurance, type_env, nat_destinataire, motif_exo, nat_prestation, code_secteur, taux_remb, type_remb
 FROM (
-    SELECT DISTINCT nat_assurance, nat_accident, type_env, nat_destinataire, motif_exo, nat_prestation, code_secteur, taux_remb, type_remb
-    FROM stg.month1
+    SELECT DISTINCT nat_assurance, type_env, nat_destinataire, motif_exo, nat_prestation, code_secteur, taux_remb, type_remb
+    FROM stg.transactions_libelles
 );
 
 CREATE OR REPLACE TABLE dwh.fct_soins AS
@@ -37,51 +35,31 @@ SELECT
     dp.id_periode,
     db.id_beneficiaire,
     dpr.id_prestation,
-    m1.mnt_depassement,
-    m1.mnt_pay,
-    m1.mnt_rem
-FROM stg.month1 m1
+    tl.mnt_depassement,
+    tl.mnt_pay,
+    tl.mnt_rem
+FROM stg.transactions_libelles tl
 JOIN dwh.dim_periode dp
-  ON dp.cd_periode = MD5(CONCAT(m1.ann_soin, m1.mois_soin))
+  ON dp.cd_periode = MD5(CONCAT(tl.ann_soin, tl.mois_soin))
 JOIN dwh.dim_beneficiaire db
-  ON db.cd_beneficiaire = MD5(CONCAT(m1.age, m1.region_ben, m1.sexe, m1.ticket_mod))
+  ON db.cd_beneficiaire = MD5(CONCAT(tl.age, tl.region_ben, tl.sexe, tl.ticket_mod))
 JOIN dwh.dim_prestation dpr
   ON dpr.cd_prestation = MD5(CONCAT(
-      m1.nat_assurance, m1.nat_accident, m1.type_env,
-      m1.nat_destinataire, m1.motif_exo, m1.nat_prestation,
-      m1.code_secteur, m1.taux_remb, m1.type_remb
+      tl.nat_assurance, tl.type_env,
+      tl.nat_destinataire, tl.motif_exo, tl.nat_prestation,
+      tl.code_secteur, tl.taux_remb, tl.type_remb
   ));
 
 FROM dwh.fct_soins;
 
-
+/*
 COPY dwh.dim_periode TO 'C:\Projet\dwh.dim_periode.parquet' (FORMAT parquet);
 COPY dwh.dim_beneficiaire TO 'C:\Projet\dwh.dim_beneficiaire.parquet' (FORMAT parquet);
 COPY dwh.dim_prestation TO 'C:\Projet\dwh.dim_prestation.parquet' (FORMAT parquet);
 COPY dwh.fct_soins TO 'C:\Projet\dwh.fct_soins.parquet' (FORMAT parquet);
+*/
 
-
-SELECT
-	ann_soin,
-	MAX(mnt_depassement)
-FROM dwh.fct_soins
-JOIN dwh.dim_periode ON dwh.fct_soins.id_periode = dwh.dim_periode.id_periode
-GROUP BY ann_soin
-ORDER BY MAX(mnt_depassement) DESC;
-
-SELECT 
-	age,
-	region_ben,
-	max(mnt_pay)
-FROM dwh.fct_soins
-JOIN dwh.dim_beneficiaire ON dwh.fct_soins.id_beneficiaire = dwh.dim_beneficiaire.id_beneficiaire 
-GROUP BY age, region_ben 
-HAVING age > 50
-ORDER BY age, region_ben;
-
-
-SELECT ann_soin, COUNT(*)
-FROM dwh.fct_soins
-JOIN dwh.dim_periode on projet.dwh.fct_soins.id_periode = projet.dwh.dim_periode.id_periode 
-GROUP BY ann_soin
-ORDER BY ann_soin;
+COPY dwh.dim_periode TO 'C:\Projet\dwh.dim_periode.csv' (HEADER, DELIMITER ',');
+COPY dwh.dim_beneficiaire TO 'C:\Projet\dwh.dim_beneficiaire.csv' (HEADER, DELIMITER ',');
+COPY dwh.dim_prestation TO 'C:\Projet\dwh.dim_prestation.csv' (HEADER, DELIMITER ',');
+COPY dwh.fct_soins TO 'C:\Projet\dwh.fct_soins.csv' (HEADER, DELIMITER ',');
