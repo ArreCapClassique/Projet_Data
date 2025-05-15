016cb545c161a92cbfdc4f98cc68c130c678b84d
乃0你带我走吧 <57453310+ArreCapClassique@users.noreply.github.com>
2025-05-15 12:15:48 +0200

    pct donnes

---
+++ pct_donnees.sql
@@ -0,0 +1,56 @@
+SELECT
+    'type_envlp' AS column_name,
+    COUNT(*) AS nb_total,
+    COUNT(*) FILTER (WHERE type_envlp IN (9 ,98)) AS nb_unutilisable,
+    ROUND(
+        CAST(COUNT(*) FILTER (WHERE type_envlp IN (9 ,98)) AS FLOAT) / COUNT(*), 6
+    ) AS pct_unutilisable
+FROM stg.transactions
+UNION ALL
+SELECT
+    'motif_exo' AS column_name,
+    COUNT(*) AS nb_total,
+    COUNT(*) FILTER (WHERE motif_exo = 0 or motif_exo = 99) AS nb_unutilisable,
+    ROUND(
+        CAST(COUNT(*) FILTER (WHERE motif_exo = 0 or motif_exo = 99) AS FLOAT) / COUNT(*), 6
+    ) AS pct_unutilisable
+FROM stg.transactions
+UNION ALL
+SELECT
+    'ticket_mod' AS column_name,
+    COUNT(*) AS nb_total,
+    COUNT(*) FILTER (WHERE ticket_mod = 0 or ticket_mod = 9) AS nb_unutilisable,
+    ROUND(
+        CAST(COUNT(*) FILTER (WHERE ticket_mod = 0 or ticket_mod = 9) AS FLOAT) / COUNT(*), 6
+    ) AS pct_unutilisable
+FROM stg.transactions
+UNION ALL
+SELECT
+    'nat_destinataire' AS column_name,
+    COUNT(*) AS nb_total,
+    COUNT(*) FILTER (WHERE nat_destinataire = 0 or nat_destinataire = 99) AS nb_unutilisable,
+    ROUND(
+        CAST(COUNT(*) FILTER (WHERE nat_destinataire = 0 or nat_destinataire = 99) AS FLOAT) / COUNT(*), 6
+    ) AS pct_unutilisable
+FROM stg.transactions
+UNION ALL
+SELECT
+    'code_secteur' AS column_name,
+    COUNT(*) AS nb_total,
+    COUNT(*) FILTER (WHERE code_secteur = 9) AS nb_unutilisable,
+    ROUND(
+        CAST(COUNT(*) FILTER (WHERE code_secteur = 9) AS FLOAT) / COUNT(*), 6
+    ) AS pct_unutilisable
+FROM stg.transactions
+UNION ALL
+SELECT
+    'type_remb' AS column_name,
+    COUNT(*) AS nb_total,
+    COUNT(*) FILTER (WHERE type_remb = 99) AS nb_unutilisable,
+    ROUND(
+        CAST(COUNT(*) FILTER (WHERE type_remb = 99) AS FLOAT) / COUNT(*), 6
+    ) AS pct_unutilisable
+FROM stg.transactions;
+
+
+
