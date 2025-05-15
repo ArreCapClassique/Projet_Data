63129f0f919bf8ac2fc13787a3bbb1097b2adf14
乃0你带我走吧 <57453310+ArreCapClassique@users.noreply.github.com>
2025-05-15 12:50:06 +0200

    ajout_colonne

--- type_donnee.sql
+++ type_donnee.sql
@@ -165,4 +165,22 @@ SELECT
         WHEN type_remb = '99' THEN 'VALEUR INCONNUE'
     ELSE NULL
 END AS type_remb
+    CASE
+        WHEN cpl_cod = '0' THEN 'SANS MAJORATION'
+        WHEN cpl_cod = '1' THEN 'MAJORATION DE NUIT'
+        WHEN cpl_cod = '2' THEN 'MAJORATION DE DIMANCHE ET JOUR FERIE'
+        WHEN cpl_cod = '3' THEN 'MAJORATION D URGENCE'
+        WHEN cpl_cod = '9' THEN 'VALEUR INCONNUE'
+        ELSE NULL
+    END AS compl_acte,
+    CASE
+        WHEN prs_fjh_typ = '0' THEN 'REGIME'
+        WHEN prs_fjh_typ = '1' THEN 'REGIME LOCAL ALSACE-MOSELLE'
+        WHEN prs_fjh_typ = '2' THEN 'ASSURE'
+        WHEN prs_fjh_typ = '3' THEN 'ORGANISME COMPLEMENTAIRE'
+        WHEN prs_fjh_typ = '8' THEN 'SANS OBJET'
+        WHEN prs_fjh_typ = '9' THEN 'VALEUR INCONNUE'
+        ELSE NULL
+    END AS forfait_journalier
+
 FROM stg.transactions;
