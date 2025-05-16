d82ca22cfbb20946026fff57effd312008957194
乃0你带我走吧 <57453310+ArreCapClassique@users.noreply.github.com>
2025-05-16 15:21:18 +0200

    fixed a bug

--- 2. exploratoire.sql
+++ 2. exploratoire.sql
@@ -8,70 +8,70 @@ UNION ALL
 SELECT
     'mois_soin' AS column_name,
     COUNT(*) AS nb_total,
-    COUNT(*) FILTER (WHERE ann_soin = 0) AS nb_unutilisable,
+    COUNT(*) FILTER (WHERE mois_soin = 0) AS nb_unutilisable,
     ROUND(nb_unutilisable / nb_total, 6) AS pct_unutilisable
 FROM stg.transactions
 UNION ALL
 SELECT
     'age' AS column_name,
     COUNT(*) AS nb_total,
-    COUNT(*) FILTER (WHERE ann_soin = 99) AS nb_unutilisable,
+    COUNT(*) FILTER (WHERE age = 99) AS nb_unutilisable,
     ROUND(nb_unutilisable / nb_total, 6) AS pct_unutilisable
 FROM stg.transactions
 UNION ALL
 SELECT
     'region_ben' AS column_name,
     COUNT(*) AS nb_total,
-    COUNT(*) FILTER (WHERE ann_soin = 99) AS nb_unutilisable,
+    COUNT(*) FILTER (WHERE region_ben = 99) AS nb_unutilisable,
     ROUND(nb_unutilisable / nb_total, 6) AS pct_unutilisable
 FROM stg.transactions
 UNION ALL
 SELECT
     'sexe' AS column_name,
     COUNT(*) AS nb_total,
-    COUNT(*) FILTER (WHERE ann_soin = 99) AS nb_unutilisable,
+    COUNT(*) FILTER (WHERE sexe = 99) AS nb_unutilisable,
     ROUND(nb_unutilisable / nb_total, 6) AS pct_unutilisable
 FROM stg.transactions
 UNION ALL
 SELECT
     'nat_assurance' AS column_name,
     COUNT(*) AS nb_total,
-    COUNT(*) FILTER (WHERE ann_soin IN (0, 99)) AS nb_unutilisable,
+    COUNT(*) FILTER (WHERE nat_assurance IN (0, 99)) AS nb_unutilisable,
     ROUND(nb_unutilisable / nb_total, 6) AS pct_unutilisable
 FROM stg.transactions
 UNION ALL
 SELECT
     'type_envlp' AS column_name,
     COUNT(*) AS nb_total,
-    COUNT(*) FILTER (WHERE ann_soin IN (9, 98)) AS nb_unutilisable,
+    COUNT(*) FILTER (WHERE type_envlp IN (9, 98)) AS nb_unutilisable,
     ROUND(nb_unutilisable / nb_total, 6) AS pct_unutilisable
 FROM stg.transactions
 UNION ALL
 SELECT
     'nat_destinataire' AS column_name,
     COUNT(*) AS nb_total,
-    COUNT(*) FILTER (WHERE ann_soin IN (0, 99)) AS nb_unutilisable,
+    COUNT(*) FILTER (WHERE nat_destinataire IN (0, 99)) AS nb_unutilisable,
     ROUND(nb_unutilisable / nb_total, 6) AS pct_unutilisable
 FROM stg.transactions
 UNION ALL
 SELECT
     'code_secteur' AS column_name,
     COUNT(*) AS nb_total,
-    COUNT(*) FILTER (WHERE ann_soin = 9) AS nb_unutilisable,
+    COUNT(*) FILTER (WHERE code_secteur = 9) AS nb_unutilisable,
     ROUND(nb_unutilisable / nb_total, 6) AS pct_unutilisable
 FROM stg.transactions
 UNION ALL
 SELECT
     'type_remb' AS column_name,
     COUNT(*) AS nb_total,
-    COUNT(*) FILTER (WHERE ann_soin = 99) AS nb_unutilisable,
+    COUNT(*) FILTER (WHERE type_remb = 99) AS nb_unutilisable,
     ROUND(nb_unutilisable / nb_total, 6) AS pct_unutilisable
 FROM stg.transactions
 UNION ALL
 SELECT
     'forfait_journalier' AS column_name,
     COUNT(*) AS nb_total,
-    COUNT(*) FILTER (WHERE ann_soin IN (8, 9)) AS nb_unutilisable,
+    COUNT(*) FILTER (WHERE forfait_journalier IN (8, 9)) AS nb_unutilisable,
     ROUND(nb_unutilisable / nb_total, 6) AS pct_unutilisable
 FROM stg.transactions
 ORDER BY column_name;
