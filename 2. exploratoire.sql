d9404f8eb03d23a6649aae72b62618244fc1fd95
乃0你带我走吧 <57453310+ArreCapClassique@users.noreply.github.com>
2025-05-16 13:59:26 +0200

    Jour 5

---
+++ 2. exploratoire.sql
@@ -0,0 +1,77 @@
+SELECT
+    'ann_soin' AS column_name,
+    COUNT(*) AS nb_total,
+    COUNT(*) FILTER (WHERE ann_soin IN (0, '0001')) AS nb_unutilisable,
+    ROUND(nb_unutilisable / nb_total, 6) AS pct_unutilisable
+FROM stg.transactions
+UNION ALL
+SELECT
+    'mois_soin' AS column_name,
+    COUNT(*) AS nb_total,
+    COUNT(*) FILTER (WHERE ann_soin = 0) AS nb_unutilisable,
+    ROUND(nb_unutilisable / nb_total, 6) AS pct_unutilisable
+FROM stg.transactions
+UNION ALL
+SELECT
+    'age' AS column_name,
+    COUNT(*) AS nb_total,
+    COUNT(*) FILTER (WHERE ann_soin = 99) AS nb_unutilisable,
+    ROUND(nb_unutilisable / nb_total, 6) AS pct_unutilisable
+FROM stg.transactions
+UNION ALL
+SELECT
+    'region_ben' AS column_name,
+    COUNT(*) AS nb_total,
+    COUNT(*) FILTER (WHERE ann_soin = 99) AS nb_unutilisable,
+    ROUND(nb_unutilisable / nb_total, 6) AS pct_unutilisable
+FROM stg.transactions
+UNION ALL
+SELECT
+    'sexe' AS column_name,
+    COUNT(*) AS nb_total,
+    COUNT(*) FILTER (WHERE ann_soin = 99) AS nb_unutilisable,
+    ROUND(nb_unutilisable / nb_total, 6) AS pct_unutilisable
+FROM stg.transactions
+UNION ALL
+SELECT
+    'nat_assurance' AS column_name,
+    COUNT(*) AS nb_total,
+    COUNT(*) FILTER (WHERE ann_soin IN (0, 99)) AS nb_unutilisable,
+    ROUND(nb_unutilisable / nb_total, 6) AS pct_unutilisable
+FROM stg.transactions
+UNION ALL
+SELECT
+    'type_envlp' AS column_name,
+    COUNT(*) AS nb_total,
+    COUNT(*) FILTER (WHERE ann_soin IN (9, 98)) AS nb_unutilisable,
+    ROUND(nb_unutilisable / nb_total, 6) AS pct_unutilisable
+FROM stg.transactions
+UNION ALL
+SELECT
+    'nat_destinataire' AS column_name,
+    COUNT(*) AS nb_total,
+    COUNT(*) FILTER (WHERE ann_soin IN (0, 99)) AS nb_unutilisable,
+    ROUND(nb_unutilisable / nb_total, 6) AS pct_unutilisable
+FROM stg.transactions
+UNION ALL
+SELECT
+    'code_secteur' AS column_name,
+    COUNT(*) AS nb_total,
+    COUNT(*) FILTER (WHERE ann_soin = 9) AS nb_unutilisable,
+    ROUND(nb_unutilisable / nb_total, 6) AS pct_unutilisable
+FROM stg.transactions
+UNION ALL
+SELECT
+    'type_remb' AS column_name,
+    COUNT(*) AS nb_total,
+    COUNT(*) FILTER (WHERE ann_soin = 99) AS nb_unutilisable,
+    ROUND(nb_unutilisable / nb_total, 6) AS pct_unutilisable
+FROM stg.transactions
+UNION ALL
+SELECT
+    'forfait_journalier' AS column_name,
+    COUNT(*) AS nb_total,
+    COUNT(*) FILTER (WHERE ann_soin IN (8, 9)) AS nb_unutilisable,
+    ROUND(nb_unutilisable / nb_total, 6) AS pct_unutilisable
+FROM stg.transactions
+ORDER BY column_name;
--- 2. staging.sql
+++ 2. staging.sql
@@ -1,25 +1,229 @@
 CREATE SCHEMA IF NOT EXISTS stg;
-
 CREATE OR REPLACE TABLE stg.transactions AS
 SELECT
-	FLT_DEP_MNT mnt_depassement,
-	FLT_PAI_MNT mnt_pay,
-	FLT_REM_MNT mnt_rem,
-	SOI_ANN ann_soin,
-	SOI_MOI mois_soin,
-	AGE_BEN_SNDS age,
-	BEN_RES_REG region_ben,
-	BEN_SEX_COD sexe,
-	ASU_NAT nat_assurance,
-	CPT_ENV_TYP type_envlp,
-	DRG_AFF_NAT nat_destinataire,
-    PRS_FJH_TYP forfait_journalier,
-	PRS_PPU_SEC code_secteur,
-	PRS_REM_TAU taux_remb,
-	PRS_REM_TYP type_remb
-FROM raw.A2024*
-LIMIT 5;
-
-FROM stg.transactions;
-
-
+    SUM(FLT_DEP_MNT) AS mnt_depassement,
+    SUM(FLT_PAI_MNT) AS mnt_pay,
+    SUM(FLT_REM_MNT) AS mnt_rem,
+    SOI_ANN AS ann_soin,
+    SOI_MOI AS mois_soin,
+    AGE_BEN_SNDS AS age,
+    BEN_RES_REG AS region_ben,
+    BEN_SEX_COD AS sexe,
+    ASU_NAT AS nat_assurance,
+    CPT_ENV_TYP AS type_envlp,
+    DRG_AFF_NAT AS nat_destinataire,
+    PRS_FJH_TYP AS forfait_journalier,
+    PRS_PPU_SEC AS code_secteur,
+    PRS_REM_TAU AS taux_remb,
+    PRS_REM_TYP AS type_remb
+FROM raw.A202401
+GROUP BY ALL
+UNION ALL
+SELECT
+    SUM(FLT_DEP_MNT) AS mnt_depassement,
+    SUM(FLT_PAI_MNT) AS mnt_pay,
+    SUM(FLT_REM_MNT) AS mnt_rem,
+    SOI_ANN AS ann_soin,
+    SOI_MOI AS mois_soin,
+    AGE_BEN_SNDS AS age,
+    BEN_RES_REG AS region_ben,
+    BEN_SEX_COD AS sexe,
+    ASU_NAT AS nat_assurance,
+    CPT_ENV_TYP AS type_envlp,
+    DRG_AFF_NAT AS nat_destinataire,
+    PRS_FJH_TYP AS forfait_journalier,
+    PRS_PPU_SEC AS code_secteur,
+    PRS_REM_TAU AS taux_remb,
+    PRS_REM_TYP AS type_remb
+FROM raw.A202402
+GROUP BY ALL
+UNION ALL
+SELECT
+    SUM(FLT_DEP_MNT) AS mnt_depassement,
+    SUM(FLT_PAI_MNT) AS mnt_pay,
+    SUM(FLT_REM_MNT) AS mnt_rem,
+    SOI_ANN AS ann_soin,
+    SOI_MOI AS mois_soin,
+    AGE_BEN_SNDS AS age,
+    BEN_RES_REG AS region_ben,
+    BEN_SEX_COD AS sexe,
+    ASU_NAT AS nat_assurance,
+    CPT_ENV_TYP AS type_envlp,
+    DRG_AFF_NAT AS nat_destinataire,
+    PRS_FJH_TYP AS forfait_journalier,
+    PRS_PPU_SEC AS code_secteur,
+    PRS_REM_TAU AS taux_remb,
+    PRS_REM_TYP AS type_remb
+FROM raw.A202403
+GROUP BY ALL
+UNION ALL
+SELECT
+    SUM(FLT_DEP_MNT) AS mnt_depassement,
+    SUM(FLT_PAI_MNT) AS mnt_pay,
+    SUM(FLT_REM_MNT) AS mnt_rem,
+    SOI_ANN AS ann_soin,
+    SOI_MOI AS mois_soin,
+    AGE_BEN_SNDS AS age,
+    BEN_RES_REG AS region_ben,
+    BEN_SEX_COD AS sexe,
+    ASU_NAT AS nat_assurance,
+    CPT_ENV_TYP AS type_envlp,
+    DRG_AFF_NAT AS nat_destinataire,
+    PRS_FJH_TYP AS forfait_journalier,
+    PRS_PPU_SEC AS code_secteur,
+    PRS_REM_TAU AS taux_remb,
+    PRS_REM_TYP AS type_remb
+FROM raw.A202404
+GROUP BY ALL
+UNION ALL
+SELECT
+    SUM(FLT_DEP_MNT) AS mnt_depassement,
+    SUM(FLT_PAI_MNT) AS mnt_pay,
+    SUM(FLT_REM_MNT) AS mnt_rem,
+    SOI_ANN AS ann_soin,
+    SOI_MOI AS mois_soin,
+    AGE_BEN_SNDS AS age,
+    BEN_RES_REG AS region_ben,
+    BEN_SEX_COD AS sexe,
+    ASU_NAT AS nat_assurance,
+    CPT_ENV_TYP AS type_envlp,
+    DRG_AFF_NAT AS nat_destinataire,
+    PRS_FJH_TYP AS forfait_journalier,
+    PRS_PPU_SEC AS code_secteur,
+    PRS_REM_TAU AS taux_remb,
+    PRS_REM_TYP AS type_remb
+FROM raw.A202405
+GROUP BY ALL
+UNION ALL
+SELECT
+    SUM(FLT_DEP_MNT) AS mnt_depassement,
+    SUM(FLT_PAI_MNT) AS mnt_pay,
+    SUM(FLT_REM_MNT) AS mnt_rem,
+    SOI_ANN AS ann_soin,
+    SOI_MOI AS mois_soin,
+    AGE_BEN_SNDS AS age,
+    BEN_RES_REG AS region_ben,
+    BEN_SEX_COD AS sexe,
+    ASU_NAT AS nat_assurance,
+    CPT_ENV_TYP AS type_envlp,
+    DRG_AFF_NAT AS nat_destinataire,
+    PRS_FJH_TYP AS forfait_journalier,
+    PRS_PPU_SEC AS code_secteur,
+    PRS_REM_TAU AS taux_remb,
+    PRS_REM_TYP AS type_remb
+FROM raw.A202406
+GROUP BY ALL
+UNION ALL
+SELECT
+    SUM(FLT_DEP_MNT) AS mnt_depassement,
+    SUM(FLT_PAI_MNT) AS mnt_pay,
+    SUM(FLT_REM_MNT) AS mnt_rem,
+    SOI_ANN AS ann_soin,
+    SOI_MOI AS mois_soin,
+    AGE_BEN_SNDS AS age,
+    BEN_RES_REG AS region_ben,
+    BEN_SEX_COD AS sexe,
+    ASU_NAT AS nat_assurance,
+    CPT_ENV_TYP AS type_envlp,
+    DRG_AFF_NAT AS nat_destinataire,
+    PRS_FJH_TYP AS forfait_journalier,
+    PRS_PPU_SEC AS code_secteur,
+    PRS_REM_TAU AS taux_remb,
+    PRS_REM_TYP AS type_remb
+FROM raw.A202407
+GROUP BY ALL
+UNION ALL
+SELECT
+    SUM(FLT_DEP_MNT) AS mnt_depassement,
+    SUM(FLT_PAI_MNT) AS mnt_pay,
+    SUM(FLT_REM_MNT) AS mnt_rem,
+    SOI_ANN AS ann_soin,
+    SOI_MOI AS mois_soin,
+    AGE_BEN_SNDS AS age,
+    BEN_RES_REG AS region_ben,
+    BEN_SEX_COD AS sexe,
+    ASU_NAT AS nat_assurance,
+    CPT_ENV_TYP AS type_envlp,
+    DRG_AFF_NAT AS nat_destinataire,
+    PRS_FJH_TYP AS forfait_journalier,
+    PRS_PPU_SEC AS code_secteur,
+    PRS_REM_TAU AS taux_remb,
+    PRS_REM_TYP AS type_remb
+FROM raw.A202408
+GROUP BY ALL
+UNION ALL
+SELECT
+    SUM(FLT_DEP_MNT) AS mnt_depassement,
+    SUM(FLT_PAI_MNT) AS mnt_pay,
+    SUM(FLT_REM_MNT) AS mnt_rem,
+    SOI_ANN AS ann_soin,
+    SOI_MOI AS mois_soin,
+    AGE_BEN_SNDS AS age,
+    BEN_RES_REG AS region_ben,
+    BEN_SEX_COD AS sexe,
+    ASU_NAT AS nat_assurance,
+    CPT_ENV_TYP AS type_envlp,
+    DRG_AFF_NAT AS nat_destinataire,
+    PRS_FJH_TYP AS forfait_journalier,
+    PRS_PPU_SEC AS code_secteur,
+    PRS_REM_TAU AS taux_remb,
+    PRS_REM_TYP AS type_remb
+FROM raw.A202409
+GROUP BY ALL
+UNION ALL
+SELECT
+    SUM(FLT_DEP_MNT) AS mnt_depassement,
+    SUM(FLT_PAI_MNT) AS mnt_pay,
+    SUM(FLT_REM_MNT) AS mnt_rem,
+    SOI_ANN AS ann_soin,
+    SOI_MOI AS mois_soin,
+    AGE_BEN_SNDS AS age,
+    BEN_RES_REG AS region_ben,
+    BEN_SEX_COD AS sexe,
+    ASU_NAT AS nat_assurance,
+    CPT_ENV_TYP AS type_envlp,
+    DRG_AFF_NAT AS nat_destinataire,
+    PRS_FJH_TYP AS forfait_journalier,
+    PRS_PPU_SEC AS code_secteur,
+    PRS_REM_TAU AS taux_remb,
+    PRS_REM_TYP AS type_remb
+FROM raw.A202410
+GROUP BY ALL
+UNION ALL
+SELECT
+    SUM(FLT_DEP_MNT) AS mnt_depassement,
+    SUM(FLT_PAI_MNT) AS mnt_pay,
+    SUM(FLT_REM_MNT) AS mnt_rem,
+    SOI_ANN AS ann_soin,
+    SOI_MOI AS mois_soin,
+    AGE_BEN_SNDS AS age,
+    BEN_RES_REG AS region_ben,
+    BEN_SEX_COD AS sexe,
+    ASU_NAT AS nat_assurance,
+    CPT_ENV_TYP AS type_envlp,
+    DRG_AFF_NAT AS nat_destinataire,
+    PRS_FJH_TYP AS forfait_journalier,
+    PRS_PPU_SEC AS code_secteur,
+    PRS_REM_TAU AS taux_remb,
+    PRS_REM_TYP AS type_remb
+FROM raw.A202411
+GROUP BY ALL
+UNION ALL
+SELECT
+    SUM(FLT_DEP_MNT) AS mnt_depassement,
+    SUM(FLT_PAI_MNT) AS mnt_pay,
+    SUM(FLT_REM_MNT) AS mnt_rem,
+    SOI_ANN AS ann_soin,
+    SOI_MOI AS mois_soin,
+    AGE_BEN_SNDS AS age,
+    BEN_RES_REG AS region_ben,
+    BEN_SEX_COD AS sexe,
+    ASU_NAT AS nat_assurance,
+    CPT_ENV_TYP AS type_envlp,
+    DRG_AFF_NAT AS nat_destinataire,
+    PRS_FJH_TYP AS forfait_journalier,
+    PRS_PPU_SEC AS code_secteur,
+    PRS_REM_TAU AS taux_remb,
+    PRS_REM_TYP AS type_remb
+FROM raw.A202412
+GROUP BY ALL;
